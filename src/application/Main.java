f5ee7623fe8a3916a03efee5cc45b0143b46b8c3
Rafael D Rissmann <rafariss@gmail.com>
2020-03-03 21:37:53 -0300

    Montagem Menu inicial

--- src/application/Main.java
+++ src/application/Main.java
@@ -4,8 +4,8 @@ import java.io.IOException;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -13,8 +13,11 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
-			Parent parent = loader.load();
-			Scene mainScene = new Scene(parent);
+			ScrollPane scrollpanel = loader.load();
+			
+			scrollpanel.setFitToHeight(true);
+			scrollpanel.setFitToWidth(true);
+			Scene mainScene = new Scene(scrollpanel);
 			primaryStage.setScene(mainScene);
 			primaryStage.setTitle("LJ JavaFX ctrlEstoque");
 			primaryStage.show();
